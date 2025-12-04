7d4dd43e0571ce10846c503b3f21cd707f87c29c
deluf <67149013+deluf@users.noreply.github.com>
2025-12-04 14:06:42 +0100

    Adjusted simulation probabilities

--- segregation_system/data_coverage_view.py
+++ segregation_system/data_coverage_view.py
@@ -70,7 +70,7 @@ class DataCoverageView:
         """
         # Are features well distributed?
         if service_flag:
-            features_well_distributed = random.choice([True, False])  # TODO Probabilities
+            features_well_distributed = random.choices([True, False], [0.33, 0.67])[0]
             print(
                 f"[DataCoverageView] Simulated user decision: "
                 f"features {"not " if not features_well_distributed else " "}distributed")
